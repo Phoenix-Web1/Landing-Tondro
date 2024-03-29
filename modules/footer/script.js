818e7accdefbecbadfa2ed68f6446facf315cb29
Ashkan <ashkansalehpoor81@gmail.com>
2024-03-29 15:36:21 +0330

    Implement JavaScript function to create flexible footer
     Added a JavaScript function named createFooter() to dynamically generate a flexible footer element within the HTML document. The function creates a footer element with the ID flexibleFooter and appends it to the body of the document, facilitating the creation and customization of the footer component through dynamic scripting.

---
+++ modules/footer/script.js
@@ -0,0 +1,8 @@
+// Function to create footer
+function createFooter() {
+    // Create footer element
+    let footer = document.createElement("footer");
+    footer.id = "flexibleFooter";
+    document.body.appendChild(footer);
+  }
+  
