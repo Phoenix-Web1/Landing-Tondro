5886700044cdce325371a3b7c04f7a610e3a0e82
Alireza-Zeynabi <zeynabialireza@gmail.com>
2024-04-04 17:31:56 +0330

    validate textContents

--- modules/section-1/dynamicGenerator.js
+++ modules/section-1/dynamicGenerator.js
@@ -1,13 +1,19 @@
-function createDynamicElement (tagName,attributes,textContent,children) {
+function createDynamicElement(tagName, attributes, textContent, children) {
 
     var element = document.createElement(tagName);
 
     if (attributes) {
         for (var key in attributes) {
-          if (attributes.hasOwnProperty(key)) {
-            element.setAttribute(key, attributes[key]);
-          }
+            if (attributes.hasOwnProperty(key)) {
+                element.setAttribute(key, attributes[key]);
+            }
         }
-      }
+    }
+
+    if (textContent) {
+        var textNode = document.createTextNode(textContent);
+        element.appendChild(textNode);
+    }
     
+
 }
