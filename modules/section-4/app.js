88c5d4f842f86b09b87556b4bfe7421e9cc65bcf
mohammad_rz1 <mohammadrzx148@gmail.com>
2024-04-05 16:20:20 +0300

    add js for SEC4

---
+++ modules/section-4/app.js
@@ -0,0 +1,10 @@
+let containerSEC4 = document.createElement("section");
+containerSEC4.setAttribute("id","map");
+document.body.appendChild(containerSEC4);
+containerSEC4.innerHTML = `
+<div class="map-cart">
+    <span class="card-title"> همین حالا خرید کن</span>
+    <span class="card-des"> وظایف سفیران شامل جمع‌آوری سفارشات، تحویل به موقع، ارتباط مؤثر با مشتریان و حفظ کیفیت خدمات است.</span>
+    <button>درخواست سفیر</button>
+</div>
+`;
